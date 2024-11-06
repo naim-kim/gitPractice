6dcb4b3db69922ead5d126f443c97c8568863944
김민제 <minje9370@handong.ac.kr>
2024-11-06 22:07:05 +0900

    patch

--- src/main/java/com/example/git_study/GitController.java
+++ src/main/java/com/example/git_study/GitController.java
@@ -1,5 +1,6 @@
 package com.example.git_study;
 
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +10,10 @@ public class GitController {
     public String post(){
         return "post";
     }
+
+    @PatchMapping("/git")
+    public String patch(){
+        return "patch";
+    }
+
 }
